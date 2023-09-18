0ec0cf81b0fea3e17891937b0787e550d98b30f3
Lily Carroll <lily.carroll@stu.ocsb.ca>
2023-09-18 23:45:44 +0000

    Updated to have necessary requirements

--- hello_world_style.py
+++ hello_world_style.py
@@ -1,1 +1,13 @@
-print("Hello, World! With Style")
+#!/usr/bin/env python3
+# Created By: Lily Carroll
+# Date: Sept/18/2023
+# This program displays Hello, World! with proper style.
+
+
+def main():
+    print("Hello, World! with proper style!")
+    print("I am using a function in my code.")
+
+
+if __name__ == "__main__":
+    main()
